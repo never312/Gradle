dec06617860cd012f6d94c3e308cf6d100e36052
never312 <boss.vvap@bk.ru>
2023-03-13 19:44:40 +0300

    first commit

---
+++ src/main/java/com/never/learn.java
@@ -0,0 +1,7 @@
+package com.never;
+
+public class learn {
+    public static void main(String[] args) {
+        System.out.println("so hard!!!");
+    }
+}
---
+++ src/test/java/com/never/learnTest.java
@@ -0,0 +1,12 @@
+package com.never;
+
+import org.junit.Test;
+
+public class learnTest {
+
+    @Test
+    public void verifyNoExceptionTrhown(){
+        learn.main(new String[]{});
+    }
+
+}
